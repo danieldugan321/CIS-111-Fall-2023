e7a3d52fcb6eae9865974c4ae33bfdb4106256b1
Daniel Dugan <92039820+danieldugan321@users.noreply.github.com>
2023-12-15 18:12:10 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.example.superpassman;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Initialize the database connection
+        DatabaseConnector databaseConnector = new DatabaseConnector();
+
+        // Initialize the PassLogic with the database connection
+        PassLogic passLogic = new PassLogic(databaseConnector);
+
+        // Start the PasswordManagerUI with the initialized PassLogic
+        PasswordManagerUI passwordManagerUI = new PasswordManagerUI(passLogic);
+        passwordManagerUI.start(primaryStage);
+    }
+}
